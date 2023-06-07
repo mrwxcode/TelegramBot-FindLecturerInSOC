28ba60962b69cf00250b8c7e57026dc14f1d7d7f
Mr. WX <07092gwx@gmail.com>
2023-06-07 22:51:56 +0800

    Update

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"my.uum","l":"FindSOCLecturerBot"},{"p":"my.uum","l":"Lecturer"},{"p":"my.uum","l":"LecturerInfo"},{"p":"my.uum","l":"Main"},{"p":"my.uum","l":"MainTest"},{"p":"my.uum","l":"SQLConnection"}];updateSearchResults();
